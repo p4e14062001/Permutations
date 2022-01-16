472c3b8315b7ff05accca793d0801287cb93a7a7
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-16 13:47:08 +0530

    commit

---
+++ Permutations Without Duplicates/LintCode.py
@@ -0,0 +1,23 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: A list of integers.
+    @return: A list of permutations.
+    """
+    def permutate(self, s, ans, perm):
+        if len(s) == 0:
+            perm.append(ans)
+        else:
+            for i in range(len(s)):
+                ch = [s[i]]
+                left = s[:i]
+                right = s[i+1:]
+                s_mod = left + right
+                ans_mod = ans + ch
+                self.permutate(s_mod, ans_mod, perm)
+        return perm
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return self.permutate(nums, [], [])
